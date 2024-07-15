cab5e2fee6b788742271d1d1461ecb369bea4d3d
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-15 13:55:54 +0330

    add project

---
+++ src/components/Ticket/Ticket.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Ticket.css";
+
+export default class Ticket extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            countriesData: {
+                Iran: ["Tabriz", "Tehran", "Shiraz", "Esfahan", "Mashhad"],
+                Turkey: ["Istanbul", "Ezmir", "Ankara", "Antaliya"],
+                US: ["Los Angles", "San Diego", "Chicago"],
+            },
+            mainCountryCities: []
+        }
+    }
+
+    selectHandler(e){
+    console.log(e);
+   }
+    render() {
+        return (
+            <div className="container">
+                <div className="col-md-6 box">
+                    <input className="fnameInput" placeholder="First Name" />
+                </div>
+
+                <div className="col-md-6 box">
+                    <input className="lnameInput" placeholder="Last Name" />
+                </div>
+                <div className="col-md-6 box">
+                    <input className="phoneInput" placeholder="Phone Number" />
+                </div>
+                <div className="col-md-6 box">
+                    <input className="emailInput" placeholder="Email" />
+                </div>
+                <div className="col-md-6 box">
+                    <select className="countrySelect" onChange={(event)=>{this.selectHandler(event).bind(this)}}>
+                        <option value="-1">Please Select ...</option>
+                        <option className="option" value="Iran">Iran</option>
+                        <option className="option" value="Turkey">Turkey</option>
+                        <option className="option" value="US">United State</option>
+                    </select>
+                </div>
+                <div className="col-md-6 box">
+                    <select className="citySelect">
+                            <option value="-1">Please Select City</option>
+                    </select>
+                </div>
+                <div className="col-md-12 box">
+                    <button className="btn">Book a ticket</button>
+                </div>
+            </div>
+
+        )
+    }
+}
